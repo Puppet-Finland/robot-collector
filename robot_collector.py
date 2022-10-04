499e13b3ff03abc0bbf4c5d07a92f41d6aa5428b
Samuli Seppänen <samuli@openvpn.net>
2022-10-04 14:47:45 +0300

    Minimal conversion into a class
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- robot_collector.py
+++ robot_collector.py
@@ -1,48 +1,55 @@
 #!/usr/bin/env python
 #
+import getopt
 import xml.etree.ElementTree as ET
 
 _RESULTS_FILE = '/home/robot/output.xml'
 _METRICS_FILE = '/var/lib/node_exporter/textfile_collector/robot_result.prom'
 _TEST_APP     = 'myapp'
 
-def write_metric(name: str, desc: str, val: int, labels: str = '', type: str = 'gauge'):
-    with open(_METRICS_FILE, 'a') as f:
-        f.write(f"# HELP {name} {desc}\n")
-        f.write(f"# TYPE {name} {type}\n")
-        f.write(f"{name}{labels} {val}\n")
+class PrometheusCollector():
 
+    def __init__(self):
+        """TODO"""
 
-def clear_metrics():
-    with open(_METRICS_FILE, 'w') as f:
-        pass
-
-
-def run():
-    clear_metrics()
-
-    tree  = ET.parse(_RESULTS_FILE)
-    root  = tree.getroot()
-
-    test_count = 0
-
-    for stat in root.find('statistics').find('suite').findall('stat'):
-        stat_id     = stat.get('id')
-        stat_name   = stat.get('name')
-        test_count += 1
-
-        failed  = stat.get('fail')
-        skipped = stat.get('skip')
-        passed  = stat.get('pass')
-
-        labels=f'{{test_app="{_TEST_APP}", test_id="{stat_id}", test_name="{stat_name}"}}'
-
-        write_metric('robot_passed_total', 'Total number of passed tests.', passed, labels)
-        write_metric('robot_skipped_total', 'Total number of skipped tests.', skipped, labels)
-        write_metric('robot_failed_total', 'Total number of failed tests.', failed, labels)
-
-
-    write_metric('robot_tests_total', 'Total number of all tests.', test_count, f'{{test_app="{_TEST_APP}"}}')
+    def write_metric(self, name: str, desc: str, val: int, labels: str = '', type: str = 'gauge'):
+        with open(_METRICS_FILE, 'a') as f:
+            f.write(f"# HELP {name} {desc}\n")
+            f.write(f"# TYPE {name} {type}\n")
+            f.write(f"{name}{labels} {val}\n")
+    
+    
+    def clear_metrics(self):
+        with open(_METRICS_FILE, 'w') as f:
+            pass
+    
+    
+    def run(self):
+        self.clear_metrics()
+    
+        tree  = ET.parse(_RESULTS_FILE)
+        root  = tree.getroot()
+    
+        test_count = 0
+    
+        for stat in root.find('statistics').find('suite').findall('stat'):
+            stat_id     = stat.get('id')
+            stat_name   = stat.get('name')
+            test_count += 1
+    
+            failed  = stat.get('fail')
+            skipped = stat.get('skip')
+            passed  = stat.get('pass')
+    
+            labels=f'{{test_app="{_TEST_APP}", test_id="{stat_id}", test_name="{stat_name}"}}'
+    
+            self.write_metric('robot_passed_total', 'Total number of passed tests.', passed, labels)
+            self.write_metric('robot_skipped_total', 'Total number of skipped tests.', skipped, labels)
+            self.write_metric('robot_failed_total', 'Total number of failed tests.', failed, labels)
+    
+    
+        self.write_metric('robot_tests_total', 'Total number of all tests.', test_count, f'{{test_app="{_TEST_APP}"}}')
 
 if __name__ == '__main__':
-    run()
+    collector = PrometheusCollector()
+    collector.run()
