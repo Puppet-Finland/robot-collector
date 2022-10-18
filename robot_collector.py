0cbf16e91176bdad85d61e19937a7955fbb184f4
Samuli Seppänen <samuli@openvpn.net>
2022-10-18 15:18:29 +0300

    Use "id" instead of "testsuite"
    Using "testsuite" as a command-line parameter was confusing, because a single
    output.xml can contain multiple Robot Framework test suites. So, what "id"
    (previously "testsuite") is referring to now is basically an instance of
    output.xml. The "id" parameter is mainly intended for internal use in Prometheus
    as it does not map to anything in Robot Framework itself.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- robot_collector.py
+++ robot_collector.py
@@ -6,11 +6,11 @@ import xml.etree.ElementTree as ET
 
 class PrometheusCollector():
 
-    def __init__(self, outputxml, metricsfile, testsuite):
+    def __init__(self, outputxml, metricsfile, id):
         """Initialize new instance of Robot Collector"""
         self.outputxml = outputxml
         self.metricsfile = metricsfile
-        self.testsuite = testsuite
+        self.id = id
 
     def write_metric(self, name: str, desc: str, val: int, labels: str = '', type: str = 'gauge'):
         with open(self.metricsfile, 'a') as f:
@@ -30,47 +30,46 @@ class PrometheusCollector():
         tree  = ET.parse(self.outputxml)
         root  = tree.getroot()
     
-        test_count = 0
+        suite_count = 0
     
         for stat in root.find('statistics').find('suite').findall('stat'):
             stat_id     = stat.get('id')
             stat_name   = stat.get('name')
-            test_count += 1
+            suite_count += 1
     
             failed  = stat.get('fail')
             skipped = stat.get('skip')
             passed  = stat.get('pass')
     
-            labels=f'{{suite="{self.testsuite}", suite_id="{stat_id}", suite_name="{stat_name}"}}'
+            labels=f'{{id="{self.id}", suite_id="{stat_id}", suite_name="{stat_name}"}}'
     
             self.write_metric('robot_passed_total', 'Total number of passed tests.', passed, labels)
             self.write_metric('robot_skipped_total', 'Total number of skipped tests.', skipped, labels)
             self.write_metric('robot_failed_total', 'Total number of failed tests.', failed, labels)
     
-    
-        self.write_metric('robot_tests_total', 'Total number of all tests.', test_count, f'{{suite="{self.testsuite}"}}')
+        self.write_metric('robot_testsuites_total', 'Total number of test suites.', suite_count, f'{{id="{self.id}"}}')
 
 def usage():
-    print("Usage: robot_collector [-h|--help] -o|--outputxml <path to output.xml> -m|--metricsfile <metrics file for textfile collector> -t|--testsuite <test suite name>")
+    print("Usage: robot_collector [-h|--help] -o|--outputxml <path to output.xml> -m|--metricsfile <metrics file for textfile collector> -i|--id <prometheus identifier for the test results>")
     print()
     print("Options:")
     print("  -h, --help:        print this help message")
     print("  -o, --outputxml:   path to Robot Framework-generated output.xml with the test results")
     print("  -m, --metricsfile: where to save the metrics - should be insider the Prometheus Textfile Collector directory")
-    print("  -t, --testsuite:   name of the test suite. Used as a label in the produced metrics to distinguish between test suites")
+    print("  -i, --id:          prometheus identifier for the test results: not to be confused with test suite (output.xml may have several)")
     print()
     sys.exit(2)
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:m:t:", ["help", "outputxml=", "metricsfile=", "testsuite="])
+        opts, args = getopt.getopt(sys.argv[1:], "ho:m:i:", ["help", "outputxml=", "metricsfile=", "id="])
     except getopt.GetoptError as err:
         print(err)
         usage()
 
     outputxml = None
     metricsfile = None
-    testsuite = None
+    id = None
 
     for o, a in opts:
         if o in ("-h", "--help"):
@@ -80,15 +79,15 @@ def main():
             outputxml = a
         elif o in ("-m", "--metricsfile"):
             metricsfile = a
-        elif o in ("-t", "--testsuite"):
-            testsuite = a
+        elif o in ("-i", "--id"):
+            id = a
         else:
             assert False, "unhandled option"
 
-    if outputxml == None or metricsfile == None or testsuite == None:
+    if outputxml == None or metricsfile == None or id == None:
         usage()
 
-    collector = PrometheusCollector(outputxml, metricsfile, testsuite)
+    collector = PrometheusCollector(outputxml, metricsfile, id)
     collector.run()
 
 if __name__ == '__main__':
